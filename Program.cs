a2e867fa40b1af64c4c001c53456a9973d553f73
Ilia Detchenkov <detchenkov@bk.ru>
2021-12-05 16:27:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab01_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base a = new Base();
+            Base b = new A();
+
+            Console.WriteLine(a.GetInfo());
+            Console.WriteLine(b.GetInfo());
+        }
+    }
+
+    class Base
+    {
+        public virtual string GetInfo()
+        {
+            return "Base class";
+        }
+    }
+    class A : Base
+    {
+        public override string GetInfo()
+        {
+            return "A class";
+        }
+    }
+
+}
+
